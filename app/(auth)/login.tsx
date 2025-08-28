2973a5ff5dc6b880940ad36377b45a7ed7a541f2
mahnaf <mahnaf125@gmail.com>
2025-08-28 12:32:05 +0600

    refactor: implement google login

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,9 +1,26 @@
 import { COLORS } from "@/constants/theme";
 import { styles } from "@/styles/authStyles";
+import { useSSO } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Login() {
+	const { startSSOFlow } = useSSO();
+	const router = useRouter();
+	const handleGoogleLogin = async () => {
+		try {
+			const { createdSessionId, setActive } = await startSSOFlow({
+				strategy: "oauth_google",
+			});
+			if (setActive && createdSessionId) {
+				setActive({ session: createdSessionId });
+				router.replace("/(tabs)");
+			}
+		} catch (error) {
+			console.error("Google OAuth Error:", error);
+		}
+	};
 	return (
 		<View style={styles.container}>
 			{/* Brand Section */}
@@ -34,7 +51,7 @@ export default function Login() {
 				{/* Login-with-Google button */}
 				<TouchableOpacity
 					style={styles.googleButton}
-					// onPress={handleGoogleLogin}
+					onPress={handleGoogleLogin}
 					activeOpacity={0.9}
 				>
 					{/* Google Logo Icon */}
