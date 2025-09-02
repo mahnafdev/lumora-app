46bfa3c298d7f4858bc8be14b1ed2b2f62d34598
mahnafdev <mahnafdev@gmail.com>
2025-09-02 11:32:24 +0600

    feat(query): create posts.getFeedPosts query
    Create posts.getFeedPosts query to get/fetch all posts data for feed screen.

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,6 +1,53 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getAuthenticatedUser } from "./users";
+
+// Query - Get All Posts for Feed
+export const getFeedPosts = query({
+	handler: async (ctx, args) => {
+		// Check and get the currently authenticated user
+		const currentUser = await getAuthenticatedUser(ctx);
+		// Fetch all posts from DB
+		const posts = await ctx.db.query("posts").order("desc").collect();
+		// Return an empty array if no posts in DB
+		if (posts.length === 0) return [];
+		// Organize posts with author data and interaction statuses
+		const organizedPosts = await Promise.all(
+			posts.map(async (post) => {
+				// Get the author document
+				const author = await ctx.db.get(post.authorId);
+				// Get the buzz document
+				const buzz = await ctx.db
+					.query("buzzes")
+					.withIndex("by_both", (q) =>
+						q.eq("buzzerId", currentUser._id).eq("postId", post._id),
+					)
+					.first();
+				// Get the bookmark document
+				const bookmark = await ctx.db
+					.query("bookmarks")
+					.withIndex("by_both", (q) =>
+						q.eq("bookmarkerId", currentUser._id).eq("postId", post._id),
+					)
+					.first();
+				// Return the organized post
+				return {
+					...post,
+					author: {
+						id: author?._id,
+						image: author?.image,
+						fullname: author?.fullname,
+						username: author?.username,
+					},
+					isBuzzed: !!buzz,
+					isBookmarked: !!bookmark,
+				};
+			}),
+		);
+		// Return the posts as response
+		return organizedPosts;
+	},
+});
 
 // Mutation - Generate Image Upload URL
 export const generateImageUploadUrl = mutation(
