67cd54dccc7d9b24950477109b1560e317a8d432
mahnafdev <mahnafdev@gmail.com>
2025-09-02 15:21:16 +0600

    chore: create stories mockdata for feed screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,5 @@
-import { useAuth } from "@clerk/clerk-expo";
-import { Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 
-export default function Index() {
-	const { signOut } = useAuth();
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-				rowGap: 12,
-			}}
-		>
-			<Text style={{ fontSize: 24 }}>Feed</Text>
-			<TouchableOpacity onPress={() => signOut()}>
-				<Text style={{ fontSize: 18 }}>Logout</Text>
-			</TouchableOpacity>
-		</View>
-	);
+export default function FeedScreen() {
+	return <View></View>;
 }
---
+++ constants/story_mock_data.ts
@@ -0,0 +1,50 @@
+export const STORIES = [
+	{
+		id: 1,
+		username: "You",
+		avatar: "https://images.unsplash.com/photo-1654110455429-cf322b40a906?w=200&h=200&fit=crop",
+		hasStory: false,
+	},
+	{
+		id: 2,
+		username: "aritra.codes",
+		avatar: "https://images.unsplash.com/photo-1619895862022-09114b41f16f?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+	{
+		id: 3,
+		username: "sadia_khan",
+		avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=200&h=200&fit=crop",
+		hasStory: false,
+	},
+	{
+		id: 4,
+		username: "farhan.dev",
+		avatar: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+	{
+		id: 5,
+		username: "mehnaz.designs",
+		avatar: "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+	{
+		id: 6,
+		username: "zubair.ai",
+		avatar: "https://images.unsplash.com/photo-1722322426803-101270837197?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+	{
+		id: 7,
+		username: "tahmid.gai",
+		avatar: "https://images.unsplash.com/photo-1695927621677-ec96e048dce2?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+	{
+		id: 8,
+		username: "nabila.uiux",
+		avatar: "https://images.unsplash.com/photo-1554780336-390462301acf?w=200&h=200&fit=crop",
+		hasStory: true,
+	},
+];
